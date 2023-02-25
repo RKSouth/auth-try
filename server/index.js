a3574dc327968dd5d2c35df0131e8451c13dbf5b
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2023-02-25 12:17:49 -0800

    made first route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'root',
+    database: 'auth-try'
+})
+//routes
+app.post('/create', (req,res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query('INSERT INTO users (name, email, password) VALUES (?,?,?)',
+     [name, email, password], [ err, result])
+    if (err) {
+        console.log(err)
+    } else {
+        res.send('values inserted')
+    }
+})
 
 app.get('/', (req,res) => {
     res.send('hello world')
