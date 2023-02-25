a2f80750e68ef63a4685092a9d7bddd13c239106
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2023-02-25 12:02:30 -0800

    basic express app

--- server/Routes/userRouter.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const Router=express.Router();
-const userService=require('../Services/userService');
-
-Router.post('/login',userService.userLogin);
-Router.post('/register',userService.userSignup)
-module.exports=Router
