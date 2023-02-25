dcbbfd0a7e2303d9012217e359dd962ac8148dc8
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2023-02-25 12:02:00 -0800

    added basic client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const displayInfo = () => {
+  console.log(name + ' , ' +  ' , ' + password)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>My App</h1>
       </header>
+   
+      <section className='register'>
+      <h1>register</h1>
+        <label>Name:</label>
+        <input type="text" 
+          onChange={(event) => {
+          setName(event.target.value)
+        }}
+        ></input>
+        <label>Email:</label>
+        <input type="text"  
+          onChange={(event) => {
+          setName(event.target.value)
+        }}></input>
+        <label>Password:</label>
+        <input 
+          type="password"
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+
+          ></input>
+        <button>Submit</button>
+      </section>
+      <section className='login'>
+      <h1>login</h1>
+        <label>Email:</label>
+        <input 
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+          ></input>
+        <label>Password:</label>
+        <input 
+          type="password"
+          onChange={(event) => {
+          setName(event.target.value)
+          }}
+          ></input>
+        <button onClick ={displayInfo}>GO!</button>
+      </section>
+
     </div>
   );
 }
